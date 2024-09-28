59933b7d4157bd32aa97daddc99ba9ea10f8da31
maryam <marychch19@gmail.com>
2024-09-29 02:24:36 +0330

    number

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log(
+  (document.querySelector(".message").textContent = "correct Number")
+);
+document.querySelector(".number").textContent = 13;
+
+document.querySelector(".guess");
