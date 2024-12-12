848a2b96819a0e879d8a003a9ce7522c8ab83bf5
maryam <marychch19@gmail.com>
2024-12-12 15:05:21 +0330

    number

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
-const number = Math.random();
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = number;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = document.querySelector(".guess").value;
