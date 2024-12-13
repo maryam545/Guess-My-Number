42f4fb73242e9c56f954d47eb53ad62403147bc7
maryam <marychch19@gmail.com>
2024-12-13 23:38:05 +0330

    querySelector

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const number = Math.trunc(Math.random() * 20) + 1;
 document.querySelector(".number").textContent = number;
 
 document.querySelector(".check").addEventListener("click", function () {
-  const guess = document.querySelector(".guess").value;
+  const guess = Number(document.querySelector(".guess").value);
   console.log(guess);
 });
