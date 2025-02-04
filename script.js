74636af1d08083b09f05a88518fb7e343a314ac5
maryam <marychch19@gmail.com>
2025-02-04 17:10:54 +0330

    part5

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector(".number").textContent = secretNumber;
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -9,8 +10,22 @@ document.querySelector(".check").addEventListener("click", function () {
   } else if (guess == secretNumber) {
     document.querySelector(".message").textContent = "🎇 correct number! ";
   } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "📈 too high ";
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 too high ";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "😕 you lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector(".message").textContent = "📉 too low ";
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📉 too low ";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "😕 you lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
