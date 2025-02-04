802ae81c8556753223a8f9ac4f4a261df68b97fd
maryam <marychch19@gmail.com>
2025-02-04 16:14:03 +0330

    part3

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 "use strict";
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = number;
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector(".message").textContent = "no number! ";
+  } else if (guess == number) {
+  }
 });
-if (!guess) {
-  document.querySelector(".message").textContent = "no number!";
-}
