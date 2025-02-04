0d15722415c4644da65b5cf729f6ea31630e5087
maryam <marychch19@gmail.com>
2025-02-04 16:33:54 +0330

    part4

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 "use strict";
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
   if (!guess) {
-    document.querySelector(".message").textContent = "no number! ";
-  } else if (guess == number) {
+    document.querySelector(".message").textContent = "⛔ no number! ";
+  } else if (guess == secretNumber) {
+    document.querySelector(".message").textContent = "🎇 correct number! ";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "📈 too high ";
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "📉 too low ";
   }
 });
